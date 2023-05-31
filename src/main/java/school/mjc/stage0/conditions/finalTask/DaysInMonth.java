3f4e4b808bfafa40bedcc712faf4b1982cb904b7
Kyrylo_Popov <kyrylo_popov@epam.com>
2023-05-31 11:30:55 +0300

    Module 4 final

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,24 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import java.util.Calendar;
+
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (isValidDate(year, month)) {
+            int days = getDaysInMonth(year, month);
+            System.out.println(days);
+        } else {
+            System.out.println("invalid date");
+        }
+    }
 
+    public static boolean isValidDate(int year, int month) {
+        return (year > 0) && (month >= 1 && month <= 12);
+    }
+
+    public static int getDaysInMonth(int year, int month) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, 1);
+        return calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        if (isTriangleValid(firstSide, secondSide, thirdSide)) {
+            System.out.println("this is a valid triangle");
+        } else {
+            System.out.println("it's not a triangle");
+        }
+    }
 
+    public static boolean isTriangleValid(double firstSide, double secondSide, double thirdSide) {
+        return (firstSide + secondSide > thirdSide) && (firstSide + thirdSide > secondSide) && (secondSide + thirdSide > firstSide);
     }
 }
